14070c59b88aaff8e46aecafeeee45f88be28d4c
jardelweberlessa <jardelweberlessa@gmail.com>
2025-09-12 23:09:23 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,3 @@
-import '../styles/globals.css'
-import Nav from '../components/Nav'
-
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Nav />
-      <main className="flex-1 container py-6">
-        <Component {...pageProps} />
-      </main>
-      <footer className="py-6 text-center text-xs text-gray-500">© {new Date().getFullYear()} Cerimonial SaaS (MVP)</footer>
-    </div>
-  )
+  return <Component {...pageProps} />;
 }
