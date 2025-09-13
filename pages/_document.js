da338c194b29e97229ed3f652a1bc254296872e9
jardelweberlessa <jardelweberlessa@gmail.com>
2025-09-13 00:24:53 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,13 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  const themeColor = '#111827';
   return (
     <Html lang="pt-BR">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content={themeColor} />
-
-        {/* iOS */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
-
-        <link rel="icon" href="/icons/icon-192.png" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
-        {/* registra o SW no cliente */}
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-              window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js').catch(()=>{});
-              });
-            }
-          `
-        }} />
       </body>
     </Html>
   );
